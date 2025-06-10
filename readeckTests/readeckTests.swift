910f7aa55d4ad5b1a20867987dd77d0508c6b85e
Ilyas Hallak <ihallak@neusta.de>
2025-06-10 14:11:23 +0200

    Initial Commit

---
+++ readeckTests/readeckTests.swift
@@ -0,0 +1,16 @@
+//
+//  readeckTests.swift
+//  readeckTests
+//
+//  Created by Ilyas Hallak on 10.06.25.
+//
+
+import Testing
+
+struct readeckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
