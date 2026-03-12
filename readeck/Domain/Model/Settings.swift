593edff46fe98dc9a22cf8a57151a5c3d3cc3463
Ben Hughes <ben@benrhughes.com>
2026-03-12 22:16:59 +1100

    other sort options

--- readeck/Domain/Model/Settings.swift
+++ readeck/Domain/Model/Settings.swift
@@ -36,11 +36,19 @@ struct Settings {
 enum BookmarkSortField: String, CaseIterable {
     case created = "created"
     case published = "published"
+    case domain = "domain"
+    case duration = "duration"
+    case site = "site"
+    case title = "title"
 
     var displayName: String {
         switch self {
         case .created: return "Created"
         case .published: return "Published"
+        case .domain: return "Domain"
+        case .duration: return "Duration"
+        case .site: return "Site"
+        case .title: return "Title"
         }
     }
 }
